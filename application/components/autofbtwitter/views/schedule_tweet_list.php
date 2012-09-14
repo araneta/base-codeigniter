8bd8f623019db3fa5507009f2807c62b943523fc
aldopraherda <root@aldo-desktop.(none)>
2012-09-14 20:58:13 +0700

    +upload

--- application/components/autofbtwitter/views/schedule_tweet_list.php
+++
@@ -1,1 +0,0 @@
-<div class="btn_add_akun">    <?php echo anchor('tweetscheduler/add','<strong>Add Schedule</strong>','class="bt_twitter"'); ?></div><div style="clear:both"></div>	<?phpecho $table;?>
